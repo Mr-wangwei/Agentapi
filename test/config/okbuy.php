22fcb0a8a3e4d19eff85f7632edbd5f0ebd3fe66
mrwangwei <1194023603@qq.com>
2018-12-28 16:02:00 +0800

    添加配置文件

---
+++ test/config/okbuy.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangwei
+ * Date: 2018/12/28
+ * Time: 16:00
+ */
+return [
+	//好乐买验签相关
+	'agent_config' => [
+		//服务商ID
+		'AgentId' => '',
+		//秘钥
+		'key' => '',
+		//支付密码
+		'paypwd' => '',
+	]
+];
