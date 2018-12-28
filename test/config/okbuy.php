38ec615d37a31d80ea8a476bb5efb82bd32b5cf7
Mr-wangwei <46190162+Mr-wangwei@users.noreply.github.com>
2018-12-28 16:04:42 +0800

    Update okbuy.php

--- test/config/okbuy.php
+++ test/config/okbuy.php
@@ -11,10 +11,10 @@ return [
     //好乐买验签相关
     'agent_config' => [
 	    //服务商ID
-	    'AgentId' => '18347605',
+	    'AgentId' => '',
 	    //秘钥
-	    'key' => '18347605_17610032212_c3657c2cd652c04dea29996ea2b41ba8',
+	    'key' => '',
 	    //支付密码
-	    'paypwd' => 'f379eaf3c831b04de153469d1bec345e',
+	    'paypwd' => '',
     ]
 ];
